026c1f50b2f7b4147bd0a57f0fdcf2101a65e1e6
duro86 <alvaroda86@gmail.com>
2023-03-05 18:38:43 +0100

    generaPrimos(0)

---
+++ test/numerosprimos/CribaEratostenesTest.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package numerosprimos;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Alvaro Duran <adurama436@g.educaand.es>
+ */
+public class CribaEratostenesTest {
+    
+    public CribaEratostenesTest() {
+    }
+
+    /**
+     * Test del método generarPrimos, de la clase CribaEratostenes.
+     */
+    @Test
+    public void testGenerarPrimos() {
+        System.out.println("generarPrimos");
+        int valorMax = 0;
+        
+        //Resultado esperado (arry vacío)
+        int[] expResult = new int[0];
+        
+        //Introducimos el parámetro
+        int[] result = CribaEratostenes.generarPrimos(valorMax);
+        
+        //Compara el resultado esperado con el resultado
+        assertArrayEquals(expResult, result);
+        
+    }
+    
+}
